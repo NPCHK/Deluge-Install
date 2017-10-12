ac19c9b620eebd36dc5ed5a205b2a4d09d0ebd2f
NPCHK <NPCHK@hotmail.com>
2017-10-12 11:15:14 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,7 +4,7 @@ echo "安裝Deluge-$1 ..."
 sleep 1
 
 apt-get update
-apt-get install python python-twisted python-openssl python-setuptools intltool python-xdg python-chardet geoip-database python-libtorrent python-notify python-pygame python-glade2 librsvg2-common xdg-utils python-mako -y
+apt-get install -y python python-twisted python-openssl python-setuptools intltool python-xdg python-chardet geoip-database python-libtorrent python-notify python-pygame python-glade2 librsvg2-common xdg-utils python-mako
 wget http://download.deluge-torrent.org/source/deluge-$1.tar.gz
 tar xf deluge-$1.tar.gz
 cd deluge-$1
